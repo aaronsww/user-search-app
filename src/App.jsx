5eeb323e5413699e293a0e0ce11b83139175f846
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-02-15 11:11:39 +0530

    fix: fixed the user  onChange search

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [userOrg, setUserOrg] = useState("balub");
   const [user, setUser] = useState("balub");
-  const [imageURL, setImageURL] = useState('');
+  const [imageURL, setImageURL] = useState("");
   const [name, setName] = useState(" ");
   const [bio, setBio] = useState(" ");
   const [doj, setDoj] = useState(" ");
@@ -16,63 +17,83 @@ function App() {
   const [blog, setBlog] = useState(" ");
   const [twitter, setTwitter] = useState(" ");
   const [org, setOrg] = useState(" ");
+  const [search, setSearch] = useState(0);
 
-  // function performSearch(event){
-  // event.preventDefault();
   useEffect(() => {
     axios
-      .get("https://api.github.com/users/" + user)
+      .get("https://api.github.com/users/" + userOrg)
       .then((res) => {
         console.log(res.data);
+        setUser(res.data.login)
         setImageURL(res.data.avatar_url);
         setName(res.data.name);
-        setBio(res.data.bio);
-        setBio(res.data.bio);
+        if (!res.data.bio) setBio("This profile has no bio");
+        else setBio(res.data.bio);
         setDoj(res.data.created_at);
         setRepos(res.data.public_repos);
         setFollowers(res.data.followers);
         setFollowing(res.data.following);
-        setLocation(res.data.location); 
-        console.log(res.data.blog);
-        if (!res.data.blog) setBlog("This profile has no blog");
+        if (!res.data.location) setLocation("Not Available");
+        else setLocation(res.data.location);
+        if (!res.data.blog) setBlog("Not Available");
         else setBlog(res.data.blog);
-        setTwitter(res.data.twitter_username);
-        setOrg(res.data.company);
+        if (!res.data.twitter_username) setTwitter("Not Available");
+        else setTwitter(res.data.twitter_username);
+        if (!res.data.company) setOrg("Not Available");
+        else setOrg(res.data.company);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [user]);
-  // }
+  }, [search]);
+
+  function performSearch() {
+    setSearch(search + 1);
+  }
 
   return (
     <div>
-      {/* <form> */}
       <input
         type="text"
-        value={user}
-        onChange={(e) => setUser(e.target.value)}
+        value={userOrg}
+        onChange={(e) => setUserOrg(e.target.value)}
       />
-      {/* <button type='submit' onClick={performSearch}>Search</button> */}
-      {/* </form> */}
+      <button type="submit" onClick={performSearch}>
+        Search
+      </button>
 
       <img src={imageURL} alt="" />
       <section>
-        <div>Name:{name}</div>
-        <div>Username:{user}</div>
-        <div>Bio:{bio}</div>
+        <div>
+          <div>Name:{name}</div>
+          <div>Username:{user}</div>
+          <div>Bio:{bio}</div>
+        </div>  
         <div>Date of Joining:{doj}</div>
       </section>
       <section>
-        <div>Repos:{repos}</div>
-        <div>Followers:{followers}</div>
-        <div>Folowing:{following}</div>
+        <span>
+          <div>Repos</div>
+          <div>{repos}</div>
+        </span>
+        <span>
+          <div>Followers</div>
+          <div>{followers}</div>
+        </span>
+        <span>
+          <div>Folowing</div>
+          <div>{following}</div>
+        </span>
       </section>
       <section>
-        <div>Location:{location}</div>
-        <div>Blog:{blog}</div>
-        <div>Twitter:{twitter}</div>
-        <div>Organization:{org}</div>
+        <span>
+          <div>Location:{location}</div>
+          <div>Twitter:{twitter}</div>
+        </span>
+        <span>
+          <div>Blog:{blog}</div>
+          <div>Organization:{org}</div>
+        </span>
       </section>
     </div>
   );
