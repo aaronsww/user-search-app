5285bba47ebfa726d298105a288caf1be8717341
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-02-15 10:45:33 +0530

    feat: fetches and renders image

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,62 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState('balub')
-  const [name, setName] = useState(' ')
-  const [bio, setBio] = useState(' ')
-  const [doj, setDoj] = useState(' ')
-  const [repos, setRepos] = useState(' ')
-  const [followers, setFollowers] = useState(' ')
-  const [following, setFollowing] = useState(' ')
-  const [location, setLocation] = useState(' ')
-  const [blog, setBlog] = useState(' ')
-  const [twitter, setTwitter] = useState(' ')
-  const [org, setOrg] = useState(' ')
-  
-   useEffect (() => {
-    axios.get('https://api.github.com/users/' + user)
-    .then(res => {
-      console.log(res.data)
-      setName(res.data.name)
-      setBio(res.data.bio)
-      setDoj(res.data.created_at)   
-      setRepos(res.data.public_repos)
-      setFollowers(res.data.followers)
-      setFollowing(res.data.following)
-      setLocation(res.data.location)
-      setBlog(res.data.blog)
-      setTwitter(res.data.twitter_username)
-      setOrg(res.data.company)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }, [user])
+  const [user, setUser] = useState("balub");
+  const [imageURL, setImageURL] = useState('');
+  const [name, setName] = useState(" ");
+  const [bio, setBio] = useState(" ");
+  const [doj, setDoj] = useState(" ");
+  const [repos, setRepos] = useState(" ");
+  const [followers, setFollowers] = useState(" ");
+  const [following, setFollowing] = useState(" ");
+  const [location, setLocation] = useState(" ");
+  const [blog, setBlog] = useState(" ");
+  const [twitter, setTwitter] = useState(" ");
+  const [org, setOrg] = useState(" ");
+
+  // function performSearch(event){
+  // event.preventDefault();
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/" + user)
+      .then((res) => {
+        console.log(res.data);
+        setImageURL(res.data.avatar_url);
+        setName(res.data.name);
+        setBio(res.data.bio);
+        setBio(res.data.bio);
+        setDoj(res.data.created_at);
+        setRepos(res.data.public_repos);
+        setFollowers(res.data.followers);
+        setFollowing(res.data.following);
+        setLocation(res.data.location); 
+        console.log(res.data.blog);
+        if (!res.data.blog) setBlog("This profile has no blog");
+        else setBlog(res.data.blog);
+        setTwitter(res.data.twitter_username);
+        setOrg(res.data.company);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [user]);
+  // }
 
   return (
     <div>
-     
-         <input
-        type='text'
+      {/* <form> */}
+      <input
+        type="text"
         value={user}
-        onChange={e => setUser(e.target.value)}/>
+        onChange={(e) => setUser(e.target.value)}
+      />
+      {/* <button type='submit' onClick={performSearch}>Search</button> */}
+      {/* </form> */}
 
-      <img src="" alt="" />
+      <img src={imageURL} alt="" />
       <section>
         <div>Name:{name}</div>
         <div>Username:{user}</div>
@@ -63,7 +75,7 @@ function App() {
         <div>Organization:{org}</div>
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
